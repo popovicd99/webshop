88879c7a61af21d14672a664fbec902856bbe574
Danilo Popović <90621288+popovicd99@users.noreply.github.com>
2023-04-24 11:54:02 +0200

    Dodate kolone isadmin i quantity u odgovarajuce tabele

--- webshop-back/database/migrations/2023_04_21_123510_create_ps_table.php
+++ webshop-back/database/migrations/2023_04_21_123510_create_ps_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('ps', function (Blueprint $table) {
             $table->unsignedBigInteger("product_id");
+            $table->integer("quantity");
             $table->unsignedBigInteger("size_id");
             $table->foreign("product_id")->references("id")->on("products")->onDelete('cascade');
             $table->foreign("size_id")->references("id")->on("sizes")->onDelete('cascade');
