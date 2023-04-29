5a36c0918307138dc144ee35eda3078325500615
Danilo Popović <90621288+popovicd99@users.noreply.github.com>
2023-04-29 16:11:11 +0200

    Kreirane skoro sve rute

--- webshop-front/src/App.js
+++ webshop-front/src/App.js
@@ -1,12 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router,Routes,Route,Navigate} from 'react-router-dom';
 import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Shop from './pages/Shop';
+import Checkout from './pages/Checkout';
+import SingleProduct from './pages/SingleProduct';
+import AdminPanel from './pages/AdminPanel';
+import { useState } from 'react';
+import Navbar from './components/Navbar';
+
 
 function App() {
+  const [token, setToken] = useState();
+  function addToken(token) {
+    setToken(token);
+  }
   return (
-    <>
-      <Home></Home>
-    </>
+    <Router>
+      <Routes>
+        <Route path='' element={<Navbar token={token} addToken={addToken}/>}>
+            <Route path='' element={<Home/>}/>
+            <Route path='/login' element={<Login addToken={addToken} />}/>
+            <Route path='/register' element={<Register/>}/>
+            <Route path='/shop' element={<Shop token={token}/>}/>
+            <Route path='/checkout' element={<Checkout/>}/>
+        </Route>
+        
+        
+      </Routes>
+    </Router>
   );
 }
 
