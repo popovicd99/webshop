56292ec275e4c812b30145d81c2c0a542c536442
Danilo Popović <90621288+popovicd99@users.noreply.github.com>
2023-04-30 19:21:47 +0200

    Kao fillable property dodat file_path

--- webshop-back/app/Models/Picture.php
+++ webshop-back/app/Models/Picture.php
@@ -10,10 +10,11 @@ class Picture extends Model
     use HasFactory;
 
     public $timestamps = false;
-
+    protected $fillable = [
+        'file_path'
+    ];
     public function products()
     {
-        return $this->belongsToMany(Product::class,'pp','picture_id','product_id');
+        return $this->belongsToMany(Product::class, 'pp', 'picture_id', 'product_id');
     }
-
 }
