7e48d19a6f1825e88ac8dbf227b200f44ec9429d
Danilo Popović <90621288+popovicd99@users.noreply.github.com>
2023-05-04 17:38:13 +0200

    Dodata quantity kolona i povezana sa proizvodom

--- webshop-back/app/Models/Product.php
+++ webshop-back/app/Models/Product.php
@@ -15,17 +15,16 @@ class Product extends Model
 
     public function orders()
     {
-        return $this->belongsToMany(Order::class,'order_items','product_id','order_id');
+        return $this->belongsToMany(Order::class, 'order_items', 'product_id', 'order_id');
     }
 
     public function sizes()
     {
-        return $this->belongsToMany(Size::class,'ps','product_id','size_id');
+        return $this->belongsToMany(Size::class, 'ps', 'product_id', 'size_id')->withPivot("quantity");
     }
 
     public function pictures()
     {
-        return $this->belongsToMany(Picture::class,'pp','product_id','picture_id');
+        return $this->belongsToMany(Picture::class, 'pp', 'product_id', 'picture_id');
     }
-
 }
