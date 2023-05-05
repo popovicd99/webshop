f48048e5e2d88c6e8575348ab1cbf8d6223ad222
Danilo Popović <90621288+popovicd99@users.noreply.github.com>
2023-05-05 10:05:38 +0200

    Dodato vise funkcija(vise u desc)
    Dodato postavljanje admina i usera, kao i prosledjivanje istih u potrebne komponente.
    Dodato postavljanje korpe i proizvoda koji se nalaze u korpi.
    Dodate potrebne rute za gore navedene funkcije

--- webshop-front/src/App.js
+++ webshop-front/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router,Routes,Route,Navigate} from 'react-router-dom';
+import { BrowserRouter as Router,Routes,Route} from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
@@ -16,16 +16,69 @@ function App() {
   function addToken(token) {
     setToken(token);
   }
+
+  const [admin, setAdmin] = useState();
+  function addAdmin(admin) {
+    setAdmin(admin);
+  }
+  const [user, setUser] = useState();
+  function addUser(user) {
+    setUser(user);
+  }
+  const[cartNumber,setcartNumber] = useState(0);
+  const[cartProducts,setcartProducts] = useState({
+    "products": [],
+    "sum":0
+  });
+ 
+  function addToCart(product){
+    if(cartProducts.products.length == 0){
+      cartProducts['products'].push(product);
+      setcartProducts(cartProducts);
+    }else{
+      let keyExist = cartProducts.products.some(key => key?.id === product.id);
+      if(keyExist){
+        cartProducts.products.forEach(element => {
+          if(element.id == product.id){
+            var temp={
+              'size':product.sizes[0].size,
+              'quantity':product.sizes[0].quantity
+            }
+            element.sizes.push(temp);
+          }
+        });
+        setcartProducts(cartProducts);
+      }else{
+        cartProducts['products'].push(product);
+        setcartProducts(cartProducts);
+      }
+    }
+    var sum=0;
+    var quan =0;
+    cartProducts.products.forEach(element => {
+      element.sizes.forEach(sz=>{
+        quan+=sz.quantity;
+      });
+        sum+= quan*element.price;
+        element['sum']=quan*element.price;
+        quan=0;
+      }
+    );
+    cartProducts['sum']=sum;
+    setcartProducts(cartProducts);
+    setcartNumber(cartNumber+1);
+  }
   return (
     <Router>
       <Routes>
-        <Route path='' element={<Navbar token={token} addToken={addToken}/>}>
+        <Route path='' element={<Navbar token={token} addToken={addToken} admin={admin} addAdmin={addAdmin} cartNumber={cartNumber}/>}>
             <Route path='' element={<Home/>}/>
-            <Route path='/login' element={<Login addToken={addToken} />}/>
+            <Route path='/login' element={<Login addToken={addToken} addAdmin={addAdmin} addUser={addUser}/>}/>
             <Route path='/register' element={<Register/>}/>
             <Route path='/shop' element={<Shop token={token}/>}/>
-            <Route path='/checkout' element={<Checkout/>}/>
-            <Route path='/adminpanel' element={<AdminPanel/>}/>
+            <Route path='/product/:slug' element={<SingleProduct addToCart={addToCart}/>}/>       
+            <Route path='/adminpanel' element={<AdminPanel admin={admin} token={token}/>}/>
+            <Route path='/checkout' element={<Checkout cartProducts={cartProducts} setcartNumber={setcartNumber} setcartProducts={setcartProducts} user={user}/>}/>          
         </Route>
         
         
